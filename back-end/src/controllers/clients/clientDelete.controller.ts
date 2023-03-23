565c0f3139f3fdbd5f5c27a73147d5d25a90f35f
Matheus Oliveira <matheus.oliveira199768@gmail.com>
2023-03-23 10:56:09 -0300

    fix routes without params

--- back-end/src/controllers/clients/clientDelete.controller.ts
+++ back-end/src/controllers/clients/clientDelete.controller.ts
@@ -2,6 +2,6 @@ import { Request, Response } from "express";
 import { deleteClientService } from "../../services/client/clientDelete.service";
 
 export const deleteClientController = async (req: Request, res: Response) => {
-  const deleteClient = await deleteClientService(req.params.id);
+  const deleteClient = await deleteClientService(req.client.id);
   return res.status(204).json(deleteClient);
 };
--- back-end/src/controllers/clients/clientUpdate.controller.ts
+++ back-end/src/controllers/clients/clientUpdate.controller.ts
@@ -4,7 +4,7 @@ import { clientUpdateService } from "../../services/client/clientUpdate.service"
 
 export const updateClientController = async (req: Request, res: Response) => {
   const clientData: iClientUpdate = req.body;
-  const clientId = req.params.id;
+  const clientId = req.client.id;
   const updateClient = await clientUpdateService(clientData, clientId);
 
   return res.json(updateClient);
