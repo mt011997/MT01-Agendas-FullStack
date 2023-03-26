d6626cdb932312aa0fd0efaa51a43ab90e4836cf
Matheus Oliveira <matheus.oliveira199768@gmail.com>
2023-03-26 17:40:20 -0300

    feat: add search contacts

--- front-end/src/components/HomePageContacts/index.tsx
+++ front-end/src/components/HomePageContacts/index.tsx
@@ -1,4 +1,5 @@
 import { useContext } from "react";
+import { DashBoardContext } from "../../contexts/contextDashBoard";
 import { LoginContext } from "../../contexts/contextLogin";
 import { ModalDeleteContactsContext } from "../../contexts/contextModalDeleteContacts";
 import { EditModalContactContext } from "../../contexts/contextModalEditContact";
@@ -6,32 +7,39 @@ import { LiContact, UlContact } from "./styled";
 
 export const HomePageContacts = () => {
   const { contacts } = useContext(LoginContext);
+  const { findContacts } = useContext(DashBoardContext)
   const { abrirModalDelete } = useContext(ModalDeleteContactsContext);
   const { abrirModalEditContact } = useContext(EditModalContactContext)
-
+  console.log(contacts, findContacts)
   return (
-    <UlContact>
-      {contacts.length === 0 ? (
-        <h3>Você não possui nenhum contato no momento! Adicione no botão de <b>+</b> acima! </h3>
-      ) : (
-        contacts.map((contact) => {
-          return (
-            <LiContact
+    <>
+      <div>
+        {contacts.length === 0 ? (
+          <h3>Você não possui nenhum contato no momento! Adicione no botão de <b>+</b> acima! </h3>)
+          :
+          <></>
+        }
+      </div>
+      <UlContact>
+          {(findContacts.length > 0 ? findContacts : contacts).map((contact) => {
+            return (
+              <LiContact
               key={contact.id}
-            >
-              <h2>Contato: {contact.full_name}</h2>
-              <span>Email: {contact.email}</span>
-              <span>Telefone: {contact.phone}</span>
+              >
+                <h2>Contato: {contact.full_name}</h2>
+                <span>Email: {contact.email}</span>
+                <span>Telefone: {contact.phone}</span>
                 <button onClick={() => abrirModalEditContact(contact.id)}>
                   Atualizar
                 </button>
                 <button onClick={() => abrirModalDelete(contact.full_name, contact.id)}>
                   Deletar
                 </button>
-            </LiContact>
-          );
-        })
-      )}
-    </UlContact>
+              </LiContact>
+              );
+            })
+          }
+      </UlContact>
+    </>
   );
 };
