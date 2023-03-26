678c2afd5d082eb17c190646f29dbceca1497e7f
Matheus Oliveira <matheus.oliveira199768@gmail.com>
2023-03-26 04:47:34 -0300

    feat: add contexts modals, dashboard and edit context signup

--- front-end/src/contexts/contextDashBoard/index.tsx
+++ front-end/src/contexts/contextDashBoard/index.tsx
@@ -1,16 +1,30 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { iContextProps } from "../contextCadastro";
+import { iContextProps } from "../contextSignUp";
 import { LoginContext } from "../contextLogin";
 
 interface iDashBoardContext {
   logout: () => void;
+  fecharModal: () => void;
+  modal: boolean;
+  abrirModal: () => void;
 }
+
 export const DashBoardContext = createContext({} as iDashBoardContext);
 
 export const DashBoardProvider = ({ children }: iContextProps) => {
   const navigate = useNavigate();
   const { setClient } = useContext(LoginContext);
+
+  const [modal, setModal] = useState(false);
+
+  const abrirModal = () => {
+    setModal(true);
+  };
+
+  const fecharModal = () => {
+    setModal(false);
+  };
 
   const logout = () => {
     localStorage.clear();
@@ -19,7 +33,7 @@ export const DashBoardProvider = ({ children }: iContextProps) => {
   };
 
   return (
-    <DashBoardContext.Provider value={{ logout }}>
+    <DashBoardContext.Provider value={{ logout, fecharModal, modal, abrirModal }}>
       {children}
     </DashBoardContext.Provider>
   );
